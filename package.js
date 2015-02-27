9cc49db5ad38d7d9a2edc869bae68243a30f6868
Benjamin Fox <foxbenjaminfox@gmail.com>
2015-02-27 16:51:30 +0200

    Add git url

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'bjf:spoilers',
   version: '1.0.0',
   summary: 'Use spoiler tags to avoid accidentally spilling the beans',
-  git: '',
+  git: 'https://github.com/foxbenjaminfox/meteor-spoilers',
   documentation: 'README.md'
 });
 
